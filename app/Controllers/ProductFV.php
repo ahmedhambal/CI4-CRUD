50aee78ff624caf36ef1c2fa5a5d95246c2da87d
Ahmad Hambali <hambaliahmad88@gmail.com>
2020-05-26 11:21:50 +0700

    Add form validation

--- app/Controllers/ProductFV.php
+++ app/Controllers/ProductFV.php
@@ -5,17 +5,17 @@ namespace App\Controllers;
 use CodeIgniter\Controller;
 use App\Models\Product_model;
 
-class Product extends Controller
+class ProductFV extends Controller
 {
     public function index()
     {
         $model = new Product_model();
         $data['product'] = $model->getProduct();
-        echo view('Product/product_view', $data);
+        echo view('ProductFV/product_view', $data);
     }
     public function add_new()
     {
-        echo view('Product/add_product_view');
+        echo view('ProductFV/add_product_view');
     }
 
     public function save()
@@ -26,14 +26,14 @@ class Product extends Controller
             'product_price' => $this->request->getPost('product_price'),
         );
         $model->saveProduct($data);
-        return redirect()->to('/product');
+        return redirect()->to('/productFV');
     }
 
     public function edit($id)
     {
         $model = new Product_model();
         $data['product'] = $model->getProduct($id)->getRow();
-        echo view('Product/edit_product_view', $data);
+        echo view('ProductFV/edit_product_view', $data);
     }
 
     public function update()
@@ -45,13 +45,13 @@ class Product extends Controller
             'product_price' => $this->request->getPost('product_price'),
         );
         $model->updateProduct($data, $id);
-        return redirect()->to('/product');
+        return redirect()->to('/productFV');
     }
 
     public function delete($id)
     {
         $model = new Product_model();
         $model->deleteProduct($id);
-        return redirect()->to('/product');
+        return redirect()->to('/productFV');
     }
 }
