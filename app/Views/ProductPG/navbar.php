59117870faa2d499e98e331e890e5d32b037093b
ahmedhambal <hambaliahmad88@gmail.com>
2020-06-02 15:11:02 +0700

    CI4-CRUD

--- app/Views/ProductPG/navbar.php
+++ app/Views/ProductPG/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
-    <a class="navbar-brand" href="#">CURD</a>
+    <a class="navbar-brand" href="#">CI4-CURD</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
